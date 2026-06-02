e0f90edf936dd216f90c0d21ea5a2dc8cbe6af9a
Neocle <lilian433700@gmail.com>
2026-06-02 16:46:02 +0200

    fixed docs, templates and database directories

--- MacInstaller.cmake
+++ MacInstaller.cmake
@@ -3,16 +3,16 @@ set(bundle_icon "${CMAKE_SOURCE_DIR}/resources/conducteo.icns")
 set_source_files_properties(${bundle_icon} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
 
 # Documentation.
-file (GLOB pdf_docs "${CMAKE_SOURCE_DIR}/install/documentation.pdf" )
+file (GLOB pdf_docs "${CMAKE_SOURCE_DIR}/install/documentation/documentation.pdf" )
 set_source_files_properties(${pdf_docs} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/documentation)
 
-file (GLOB pdf_docs_2 "${CMAKE_SOURCE_DIR}/install/validations.pdf" )
+file (GLOB pdf_docs_2 "${CMAKE_SOURCE_DIR}/install/documentation/validations.pdf" )
 set_source_files_properties(${pdf_docs_2} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/documentation)
 
-file (GLOB pdf_docs_3 "${CMAKE_SOURCE_DIR}/install/tutorial.pdf" )
+file (GLOB pdf_docs_3 "${CMAKE_SOURCE_DIR}/install/documentation/tutorial.pdf" )
 set_source_files_properties(${pdf_docs_3} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/documentation)
 
-file (GLOB pdf_docs_4 "${CMAKE_SOURCE_DIR}/install/quickstart.pdf" )
+file (GLOB pdf_docs_4 "${CMAKE_SOURCE_DIR}/install/documentation/quickstart.pdf" )
 set_source_files_properties(${pdf_docs_4} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/documentation)
 
 file(GLOB_RECURSE validations_data_en10211 "${CMAKE_SOURCE_DIR}/validations/data/en10211/*.c2d")
@@ -28,20 +28,20 @@ file(GLOB_RECURSE tutorial_data "${CMAKE_SOURCE_DIR}/documentation/tutorial/*.c2
 set_source_files_properties(${tutorial_data} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/examples)
 
 # RT 2012 database.
-file (GLOB rt_db "${CMAKE_SOURCE_DIR}/install/rt2012.db" )
-set_source_files_properties(${rt_db} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
+file (GLOB rt_db "${CMAKE_SOURCE_DIR}/install/data/rt2012.db" )
+set_source_files_properties(${rt_db} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/data)
 
 # Docx template.
-file (GLOB mac_docx_template "${CMAKE_SOURCE_DIR}/install/model_fr.docx" )
+file (GLOB mac_docx_template "${CMAKE_SOURCE_DIR}/install/templates/model_fr.docx" )
 set_source_files_properties(${mac_docx_template} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/docx)
 
-file (GLOB mac_docx_template_2 "${CMAKE_SOURCE_DIR}/install/model_2_fr.docx" )
+file (GLOB mac_docx_template_2 "${CMAKE_SOURCE_DIR}/install/templates/model_2_fr.docx" )
 set_source_files_properties(${mac_docx_template_2} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/docx)
 
-file (GLOB mac_docx_template_3 "${CMAKE_SOURCE_DIR}/install/model_en.docx" )
+file (GLOB mac_docx_template_3 "${CMAKE_SOURCE_DIR}/install/templates/model_en.docx" )
 set_source_files_properties(${mac_docx_template_3} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/docx)
 
-file (GLOB mac_docx_template_4 "${CMAKE_SOURCE_DIR}/install/model_2_en.docx" )
+file (GLOB mac_docx_template_4 "${CMAKE_SOURCE_DIR}/install/templates/model_2_en.docx" )
 set_source_files_properties(${mac_docx_template_4} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/docx)
 
 file(GLOB mac_translations "${CMAKE_SOURCE_DIR}/install/translations/*.json")
@@ -56,7 +56,7 @@ set_source_files_properties(${examples} PROPERTIES MACOSX_PACKAGE_LOCATION Resou
 set(MACOSXResources ${bundle_icon} ${pdf_docs} ${pdf_docs_2} ${pdf_docs_4} ${rt_db} ${mac_translations} ${examples} ${mac_docx_template} ${mac_docx_template_2} ${mac_docx_template_3} ${mac_docx_template_4} ${validations_data_en10211} ${validations_data_en10077} ${validations_data_common} ${tutorial_data} ${pdf_docs_3})
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0")
-    
+
 # set bundle informations.
 set( MACOSX_BUNDLE_INFO_STRING          "conducteö"                                                                                         )
 set( MACOSX_BUNDLE_ICON_FILE            "conducteo.icns"                                                                                    )
